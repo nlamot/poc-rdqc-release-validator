1c11d24f6b1411b2537ba00d71c33736464a2a8f
Nils Lamot <nils@prolama.be>
2023-07-06 16:12:44 +0200

    Register trigger

--- function_app.py
+++ function_app.py
@@ -3,6 +3,7 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+@app.function_name(name="ValidateRelease")
 @app.event_grid_trigger(arg_name="azeventgrid")
 def ValidateRelease(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event')
